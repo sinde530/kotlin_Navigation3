20d12466f7d94d70ceaec4378075fb2697e421e5
kimsungeun <mirxx@naver.com>
2021-04-12 19:25:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_navigation3/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.kotlin_navigation3
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Activity for cupcake order flow.
+ */
+class MainActivity : AppCompatActivity(R.layout.activity_main)
